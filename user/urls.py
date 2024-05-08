80fe0581aedce1a8dc9ef6e48b03ee6fa50d2317
BigestCola <jiuhualai@gmail.com>
2024-05-08 17:36:52 +0800

    ak

--- user/urls.py
+++ user/urls.py
@@ -7,20 +7,14 @@ from .views import user_login, user_logout
 app_name = 'user'
 
 urlpatterns = [
-    path('', views.user_home, name='user_home'),
-    path('', views.home, name='user_home'),
-    path('login/', views.user_login, name='login'),
-    path('list/', views.UserListView.as_view(), name='user_list'),
-    path('update/<int:pk>/', views.UserUpdateView.as_view(), name='user_update'),
-    path('logout/', views.user_logout, name='logout'),
-    path('subordinate/', views.subordinate_list, name='subordinate_list'),
-    path('subordinate/create/', views.subordinate_create, name='subordinate_create'),
-    path('subordinate/<int:subordinate_id>/edit/', views.subordinate_edit, name='subordinate_edit'),
-    path('subordinate/<int:subordinate_id>/delete/', views.subordinate_delete, name='subordinate_delete'),
-    path('generate_cdkey/', views.generate_cdkey, name='generate_cdkey'),
-    path('cdkey_record/', views.cdkey_record, name='cdkey_record'),
-    path('subordinate/', views.subordinate, name='subordinate'),
-    path('create/', views.UserCreate.as_view(), name='user_create'),
-    path('user/', include(('user.urls', 'user'), namespace='user')),
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('change-password/', views.change_password, name='change_password'),
+    path('generate-cdkey/', views.generate_cdkey, name='generate_cdkey'),
+    path('cdkey-records/', views.cdkey_records, name='cdkey_records'),
+    path('create/', views.user_create, name='user_create'),
+    path('update/int:pk/', views.UserUpdateView, name='user_update'),
+    path('delete/int:pk/', views.user_delete, name='user_delete'),
 
 ]
